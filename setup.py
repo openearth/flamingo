23cfdfacab8b22e89848c14b09da8df0a30dfab3
hoonhout <hoonhout@0ff102cf-ee28-4064-adf9-66d38ca607b7>
2014-05-06 07:17:01 +0000

    Added placeholder for Flamingo toolbox
    git-svn-id: https://svn.oss.deltares.nl/repos/openearthtools/trunk/python/applications/flamingo@10648 0ff102cf-ee28-4064-adf9-66d38ca607b7

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flamingo',
+    version='0.0',
+    author='Bas Hoonhout',
+    author_email='bas.hoonhout@deltares.nl',
+    packages=find_packages(),
+    description='Flamingo Image Analysis Toolbox',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
