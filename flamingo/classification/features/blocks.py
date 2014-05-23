de1ece60d4ec5314e47b584e4231aa43cf01ee4d
baart_f <baart_f@0ff102cf-ee28-4064-adf9-66d38ca607b7>
2014-05-23 15:08:32 +0000

    autocleanup with autopep8
    git-svn-id: https://svn.oss.deltares.nl/repos/openearthtools/trunk/python/applications/flamingo@10745 0ff102cf-ee28-4064-adf9-66d38ca607b7

--- flamingo/classification/features/blocks.py
+++ flamingo/classification/features/blocks.py
@@ -46,9 +46,11 @@ _TEMP_FEATURES = {'image',
                   'filled_image',
                   'coords'}
 
+
 def list_blocks():
     '''List all block extraction functions in module'''
-    return {k:v for k,v in globals().iteritems() if k.startswith('extract_blocks_')}
+    return {k: v for k, v in globals().iteritems() if k.startswith('extract_blocks_')}
+
 
 def extract_blocks(data, segments, colorspace='rgb', blocks=None):
     '''Extract all blocks in right order'''
@@ -70,7 +72,8 @@ def extract_blocks(data, segments, colorspace='rgb', blocks=None):
                                    segments,
                                    features=features,
                                    colorspace=colorspace)
-                    features_in_block[block] = [x for x in features.columns if x not in features0]
+                    features_in_block[block] = [
+                        x for x in features.columns if x not in features0]
                     features0 = features.columns
 
                     done.append(block)
@@ -86,6 +89,7 @@ def extract_blocks(data, segments, colorspace='rgb', blocks=None):
 
     return features, features_in_block
 
+
 def __extract_blocks(grayscale=True, channel=False, derived_from=()):
     '''Feature extraction decorator.'''
     def wrapper(f):
@@ -97,23 +101,26 @@ def __extract_blocks(grayscale=True, channel=False, derived_from=()):
                 if (features is None) or (features == []):
                     features = []
                     if len(derived_from) > 0:
-                        raise ValueError('Derived features, but no feature set provided')
+                        raise ValueError(
+                            'Derived features, but no feature set provided')
             else:
                 features = [features]
 
             # check if necessary features are present for derived features
             for feature in derived_from:
-                if feature not in features0: #.keys():
-                    raise ValueError('Cannot compute derived feature. Feature "%s" is missing.' % feature)
+                if feature not in features0:  # .keys():
+                    raise ValueError(
+                        'Cannot compute derived feature. Feature "%s" is missing.' % feature)
 
             # compute feature for grayscale channel
             if grayscale:
                 if colorspace.lower() == 'rgb':
                     gs = data.mean(-1)
                 elif colorspace.lower() == 'hsv':
-                    gs = data[...,-1]
+                    gs = data[..., -1]
                 else:
-                    raise ValueError('Unsupported colorspace [%s]' % colorspace)
+                    raise ValueError(
+                        'Unsupported colorspace [%s]' % colorspace)
 
                 if type(features0) is pandas.DataFrame:
                     f0 = features0.filter(derived_from)
@@ -130,12 +137,12 @@ def __extract_blocks(grayscale=True, channel=False, derived_from=()):
                 df_sum = None
                 for i in range(data.shape[-1]):
                     if type(features0) is pandas.DataFrame:
-                        f0 = features0.filter(['%s.%d' % (x,i) if '%s.%d' % (x,i) in features0.keys() else x for x in derived_from]) \
-                                      .rename(columns=lambda x: re.sub('\.\d+$','',x))
+                        f0 = features0.filter(['%s.%d' % (x, i) if '%s.%d' % (x, i) in features0.keys() else x for x in derived_from]) \
+                                      .rename(columns=lambda x: re.sub('\.\d+$', '', x))
                     else:
                         f0 = None
 
-                    df = f(data[...,i],
+                    df = f(data[..., i],
                            segments,
                            features=f0)
 
@@ -146,7 +153,8 @@ def __extract_blocks(grayscale=True, channel=False, derived_from=()):
                     else:
                         df_sum += df
 
-                    df.columns = ['%s.%d' % (x,i) for x in df.columns] # add channel index to column name
+                    # add channel index to column name
+                    df.columns = ['%s.%d' % (x, i) for x in df.columns]
 
                     features.append(df)
 
@@ -154,72 +162,86 @@ def __extract_blocks(grayscale=True, channel=False, derived_from=()):
 
                 features.append(df_sum)
 
-            # merge features for different channels and with previous feature list
+            # merge features for different channels and with previous feature
+            # list
             return merge_blocks(features)
 
         return extract
 
     return wrapper
 
+
 def _empty_block_frame(block, n=1, columns=[], data=None, fill_value=np.empty((0))):
     '''Create empty feature dataframe'''
 
     if data is None:
-        data = [{k:fill_value for k in columns} for i in range(n)]
+        data = [{k: fill_value for k in columns} for i in range(n)]
 
     #ix = pandas.MultiIndex.from_arrays([[block] * len(columns), columns], names=['block','feature'])
-    df = pandas.DataFrame(data, columns=columns, index=np.arange(n)+1)
+    df = pandas.DataFrame(data, columns=columns, index=np.arange(n) + 1)
     #df = df.set_index('label')
 
     return df
 
+
 @__extract_blocks(grayscale=True, channel=False)
 def extract_blocks_pixel(data, segments, **kwargs):
 
-    regionprops = skimage.measure.regionprops(np.asarray(segments)+1, intensity_image=data)
-    features = [{key:feature[key] for key in _PROP_PIXEL} for feature in regionprops] # if feature._slice is not None]
+    regionprops = skimage.measure.regionprops(
+        np.asarray(segments) + 1, intensity_image=data)
+    features = [{key: feature[key] for key in _PROP_PIXEL}
+                for feature in regionprops]  # if feature._slice is not None]
 
-    df = _empty_block_frame('pixel', np.max(segments)+1, _PROP_PIXEL, data=features)
+    df = _empty_block_frame(
+        'pixel', np.max(segments) + 1, _PROP_PIXEL, data=features)
 
     return df
+
 
 @__extract_blocks(grayscale=False, channel=True)
 def extract_blocks_intensity(data, segments, **kwargs):
 
-    regionprops = skimage.measure.regionprops(np.asarray(segments)+1, intensity_image=data)
-    features = [{key:feature[key] for key in _PROP_INTENSITY} for feature in regionprops] # if feature._slice is not None]
+    regionprops = skimage.measure.regionprops(
+        np.asarray(segments) + 1, intensity_image=data)
+    features = [{key: feature[key] for key in _PROP_INTENSITY}
+                for feature in regionprops]  # if feature._slice is not None]
 
-    df = _empty_block_frame('intensity', np.max(segments)+1, _PROP_INTENSITY, data=features)
+    df = _empty_block_frame(
+        'intensity', np.max(segments) + 1, _PROP_INTENSITY, data=features)
 
     return df
 
-@__extract_blocks(grayscale=True, channel=False, derived_from=['area','centroid','perimeter','convex_area'])
+
+@__extract_blocks(grayscale=True, channel=False, derived_from=['area', 'centroid', 'perimeter', 'convex_area'])
 def extract_blocks_shape(data, segments, features=[], **kwargs):
 
-    df = _empty_block_frame('shape', np.max(segments)+1, ['size','position.n','position.m','shape','holeyness'])
+    df = _empty_block_frame('shape', np.max(
+        segments) + 1, ['size', 'position.n', 'position.m', 'shape', 'holeyness'])
 
     for i, feature in features.iterrows():
 
         # size is represented by the portion of the image covered by the region
-        df.ix[i,'size'] = np.prod(segments.shape)/feature['area']
+        df.ix[i, 'size'] = np.prod(segments.shape) / feature['area']
 
-        # position is represented using the coordinates of the region center of mass normalized by the image dimensions
-        df.ix[i,'position.n'] = feature['centroid'][0]/segments.shape[0]
-        df.ix[i,'position.m'] = feature['centroid'][1]/segments.shape[1]
+        # position is represented using the coordinates of the region center of
+        # mass normalized by the image dimensions
+        df.ix[i, 'position.n'] = feature['centroid'][0] / segments.shape[0]
+        df.ix[i, 'position.m'] = feature['centroid'][1] / segments.shape[1]
 
         # shape is represented by the ratio of the area to the perimeter squared
         # seems to be the same as solidity
-        df.ix[i,'shape'] = feature['area']/(feature['perimeter']**2)
+        df.ix[i, 'shape'] = feature['area'] / (feature['perimeter'] ** 2)
 
         # holeyness is defined as the convex area divided by the area
-        df.ix[i,'holeyness'] = feature['convex_area']/feature['area']
+        df.ix[i, 'holeyness'] = feature['convex_area'] / feature['area']
 
     return df
 
-@__extract_blocks(grayscale=True, channel=True, derived_from=['bbox','image'])
+
+@__extract_blocks(grayscale=True, channel=True, derived_from=['bbox', 'image'])
 def extract_blocks_mask(data, segments, features=[], **kwargs):
 
-    df = _empty_block_frame('mask', np.max(segments)+1, ['image_masked'])
+    df = _empty_block_frame('mask', np.max(segments) + 1, ['image_masked'])
 
     for i, feature in features.iterrows():
 
@@ -227,53 +249,64 @@ def extract_blocks_mask(data, segments, features=[], **kwargs):
         minn, minm, maxn, maxm = feature['bbox']
 
         # select the pixels that are not in the image
-        mask = np.logical_not(features.ix[i,'image'])
+        mask = np.logical_not(features.ix[i, 'image'])
 
         # select the image pixels and apply the mask
-        df.ix[i,'image_masked'] = np.ma.masked_array(data[minn:maxn, minm:maxm], mask=mask)
+        df.ix[i, 'image_masked'] = np.ma.masked_array(
+            data[minn:maxn, minm:maxm], mask=mask)
 
     return df
+
 
 @__extract_blocks(grayscale=True, channel=True, derived_from=['image_masked'])
 def extract_blocks_grey(data,
-                           segments,
-                           features=[],
-                           distances=[5,7,11],
-                           angles=np.linspace(0,1*np.pi, num=6, endpoint=False),
-                           props = ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM'], **kwargs):
+                        segments,
+                        features=[],
+                        distances=[5, 7, 11],
+                        angles=np.linspace(
+                            0, 1 * np.pi, num=6, endpoint=False),
+                        props=['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM'], **kwargs):
 
-    df = _empty_block_frame('grey', np.max(segments)+1, ['grey_%s' % x for x in props])
+    df = _empty_block_frame(
+        'grey', np.max(segments) + 1, ['grey_%s' % x for x in props])
 
     for i, feature in features.iterrows():
-        greyprops = skimage.feature.greycomatrix(features.ix[i,'image_masked'],
+        greyprops = skimage.feature.greycomatrix(features.ix[i, 'image_masked'],
                                                  distances=distances,
                                                  angles=angles)
         for prop in props:
-            df.ix[i,'grey_%s' % prop] = skimage.feature.greycoprops(greyprops, prop)
+            df.ix[i, 'grey_%s' %
+                  prop] = skimage.feature.greycoprops(greyprops, prop)
 
     return df
 
-@__extract_blocks(grayscale=True, channel=True, derived_from=['image_masked','image_masked.sum'])
+
+@__extract_blocks(grayscale=True, channel=True, derived_from=['image_masked', 'image_masked.sum'])
 def extract_blocks_intensitystatistics(data, segments, features=[], histogram_bins=5, **kwargs):
 
-    df = _empty_block_frame('intensitystatistics', np.max(segments)+1, ['variance_intensity', 'mean_relative_intensity', 'variance_relative_intensity', 'histogram'])
+    df = _empty_block_frame('intensitystatistics', np.max(
+        segments) + 1, ['variance_intensity', 'mean_relative_intensity', 'variance_relative_intensity', 'histogram'])
 
     for i, feature in features.iterrows():
 
-        df.ix[i,'variance_intensity'] = features.ix[i,'image_masked'].var()
-        df.ix[i,'mean_relative_intensity'] = (features.ix[i,'image_masked'].astype('float')/features.ix[i,'image_masked.sum']).mean()
-        df.ix[i,'variance_relative_intensity'] = (features.ix[i,'image_masked'].astype('float')/features.ix[i,'image_masked.sum']).var()
+        df.ix[i, 'variance_intensity'] = features.ix[i, 'image_masked'].var()
+        df.ix[i, 'mean_relative_intensity'] = (features.ix[i, 'image_masked'].astype(
+            'float') / features.ix[i, 'image_masked.sum']).mean()
+        df.ix[i, 'variance_relative_intensity'] = (features.ix[i, 'image_masked'].astype(
+            'float') / features.ix[i, 'image_masked.sum']).var()
 
         # intensity histogram
-        counts, bins = np.histogram(features.ix[i,'image_masked'], bins=np.linspace(0, 255, endpoint=True, num=histogram_bins+1))
-        df.ix[i,'histogram'] = counts
+        counts, bins = np.histogram(features.ix[i, 'image_masked'], bins=np.linspace(
+            0, 255, endpoint=True, num=histogram_bins + 1))
+        df.ix[i, 'histogram'] = counts
 
     return df
+
 
 def merge_blocks(features):
     if len(features) > 1:
         df = features[0]
-        for i in range(1,len(features)):
+        for i in range(1, len(features)):
             df = pandas.merge(df,
                               features[i],
                               how='outer',
--- flamingo/filesys.py
+++ flamingo/filesys.py
@@ -15,15 +15,18 @@ PATH_DATASET = '/flamingo/datasets/'
 
 def set_dataset_path(fpath):
 
-    # FIXME: if it's captitalized it's a constant. 
-    # Use a config file for this or command line args 
+    # FIXME: if it's captitalized it's a constant.
+    # Use a config file for this or command line args
     PATH_DATASET = fpath
+
+
 def get_dataset_path():
     if PATH_DATASET is not None:
         if os.path.exists(PATH_DATASET):
             return PATH_DATASET
-    demopath = resource_filename(Requirement.parse("flamingo"),"data")
+    demopath = resource_filename(Requirement.parse("flamingo"), "data")
     return demopath
+
 
 def get_dataset_list():
     fpath = get_dataset_path()
@@ -31,14 +34,16 @@ def get_dataset_list():
     datasets = []
     for fname in os.listdir(fpath):
         if not fname.startswith('.') and os.path.isdir(os.path.join(get_dataset_path(), fname)):
-            datasets.append(fname);
+            datasets.append(fname)
     return datasets
 
+
 def get_image_path(ds):
-    return os.path.join(get_dataset_path(),ds)
+    return os.path.join(get_dataset_path(), ds)
+
 
 def get_image_location(ds, im):
-    return os.path.join(get_image_path(ds),im)
+    return os.path.join(get_image_path(ds), im)
 
 
 def get_image_list(ds):
@@ -48,9 +53,11 @@ def get_image_list(ds):
         if os.path.exists(fpath):
             for im in os.listdir(fpath):
                 if im.endswith('.jpg') or im.endswith('.png') or im.endswith('.jpeg'):
-                    if not im.startswith('cropped_'): # skip cropped versions of image
+                    # skip cropped versions of image
+                    if not im.startswith('cropped_'):
                         images.append(im)
     return images
+
 
 def get_export_file(ds, im=None, ext=None):
     if im is None and ext is None:
@@ -58,15 +65,16 @@ def get_export_file(ds, im=None, ext=None):
     elif im is None:
         return os.path.join(get_image_path(ds), '%s.pkl' % ext)
     elif ext is None:
-        fpath = get_image_location(ds,im)
-        return re.sub('\.[\w\d]+$','.pkl',fpath)
+        fpath = get_image_location(ds, im)
+        return re.sub('\.[\w\d]+$', '.pkl', fpath)
     else:
-        fpath = get_image_location(ds,im)
-        return re.sub('\.[\w\d]+$','.%s.pkl' % ext,fpath)
+        fpath = get_image_location(ds, im)
+        return re.sub('\.[\w\d]+$', '.%s.pkl' % ext, fpath)
+
 
 def read_export_file(ds, im, ext):
     contents = None
-    pklfile = get_export_file(ds,im,ext)
+    pklfile = get_export_file(ds, im, ext)
     if os.path.exists(pklfile):
         try:
             with open(pklfile, 'rb') as fp:
@@ -75,8 +83,9 @@ def read_export_file(ds, im, ext):
             raise IOError('Error reading file %s' % pklfile)
     return contents
 
+
 def write_export_file(ds, im, ext, contents, append=False):
-    pklfile = get_export_file(ds,im,ext)
+    pklfile = get_export_file(ds, im, ext)
 
     if append:
         if type(contents) is dict:
@@ -89,8 +98,9 @@ def write_export_file(ds, im, ext, contents, append=False):
 
     with open(pklfile, 'wb') as fp:
         pickle.dump(contents, fp, pickle.HIGHEST_PROTOCOL)
-    
-def read_log_file(ds,keys = None):
+
+
+def read_log_file(ds, keys=None):
     log = read_export_file(ds, None, 'log')
     if (keys is None) or (log is None):
         return log
@@ -101,18 +111,20 @@ def read_log_file(ds,keys = None):
     else:
         return None
 
+
 def write_log_file(ds, contents):
     return write_export_file(ds, None, 'log', contents, append=True)
 
+
 def read_image_file(ds, im, crop=True):
-    fpath = get_image_location(ds,im)
+    fpath = get_image_location(ds, im)
 
     img = None
     if os.path.exists(fpath):
 
         if crop:
-            p,f = os.path.split(fpath)
-            fpath_cropped = os.path.join(p,'cropped_' + f)
+            p, f = os.path.split(fpath)
+            fpath_cropped = os.path.join(p, 'cropped_' + f)
             if not os.path.exists(fpath_cropped):
                 img = plt.imread(fpath)
                 img = img[8:-8,:,:]
@@ -124,11 +136,13 @@ def read_image_file(ds, im, crop=True):
 
     return img
 
+
 def write_feature_files(ds, im, features, features_in_block, ext=None):
     for block, cols in features_in_block.iteritems():
         fname = __get_feature_filename(block, ext)
         cols = [c for c in cols if c in features.columns]
         write_export_file(ds, im, fname, features[cols])
+
 
 def read_feature_files(ds, im, blocks=feature_blocks.list_blocks().keys(), ext=None):
     features = []
@@ -143,6 +157,7 @@ def read_feature_files(ds, im, blocks=feature_blocks.list_blocks().keys(), ext=N
 
     return features, features_in_block
 
+
 def get_model_list(ds):
     models = []
     if ds is not None:
@@ -151,10 +166,11 @@ def get_model_list(ds):
             for m in os.listdir(fpath):
                 if m.startswith('model_') and m.endswith('.pkl') and not m.endswith('.meta.pkl'):
                     models.append(m)
-    
+
     models.sort(key=lambda x: os.stat(os.path.join(fpath, x)).st_mtime)
 
     return models
+
 
 def write_model_files(ds, models, meta, ext=''):
     if len(ext) > 0 and not ext.startswith('.'):
@@ -164,6 +180,7 @@ def write_model_files(ds, models, meta, ext=''):
         for j in range(len(model)):
             write_model_file(ds, model[j], meta[i][j], ext='.%d%s' % (j, ext))
 
+
 def write_model_file(ds, model, meta, ext=''):
     model_name = __get_model_filename(meta) + ext
 
@@ -171,20 +188,21 @@ def write_model_file(ds, model, meta, ext=''):
     fname = os.path.join(get_image_path(ds), '%s.pkl' % model_name)
     with open(fname, 'wb') as fp:
         pickle.dump(model, fp)
-    
+
     # write meta file
     fname = os.path.join(get_image_path(ds), '%s.meta.pkl' % model_name)
     with open(fname, 'wb') as fp:
         pickle.dump(meta, fp)
 
+
 def read_model_file(ds, model_name):
-    model_name = re.sub('\.pkl$','',model_name)
+    model_name = re.sub('\.pkl$', '', model_name)
 
     # write model file
     fname = os.path.join(get_image_path(ds), '%s.pkl' % model_name)
     with open(fname, 'rb') as fp:
         model = pickle.load(fp)
-    
+
     # write meta file
     fname = os.path.join(get_image_path(ds), '%s.meta.pkl' % model_name)
     with open(fname, 'rb') as fp:
@@ -192,39 +210,45 @@ def read_model_file(ds, model_name):
 
     return model, meta
 
+
 def read_default_categories(ds):
     classes = []
-    images  = get_image_list(ds)
+    images = get_image_list(ds)
     for im in images:
         c = read_export_file(ds, im, 'classes')
         if c is not None:
             classes.extend(c)
-    
+
     return list(np.unique(classes))
-    
-def is_classified(ds,im):
-    classes = read_export_file(ds,im,'classes')
+
+
+def is_classified(ds, im):
+    classes = read_export_file(ds, im, 'classes')
     if classes is not None:
         return None not in classes
     return False
 
-def is_segmented(ds,im):
-    pklfile = get_export_file(ds,im,'segments')
+
+def is_segmented(ds, im):
+    pklfile = get_export_file(ds, im, 'segments')
     return os.path.exists(pklfile)
 
-def has_features(ds,im):
-    pklfile = get_export_file(ds,im,'features')
+
+def has_features(ds, im):
+    pklfile = get_export_file(ds, im, 'features')
     return os.path.exists(pklfile)
+
 
 def __get_feature_filename(block, ext=None):
-    block = re.sub('^extract_blocks_','',block)
-    
+    block = re.sub('^extract_blocks_', '', block)
+
     if ext:
         fname = 'features.%s.%s' % (ext, block)
     else:
         fname = 'features.%s' % block
-        
+
     return fname
+
 
 def __get_model_filename(meta):
     model_name = 'model_%s_%s_I%d_B%d_%s' % (meta['model_type'].upper(),
